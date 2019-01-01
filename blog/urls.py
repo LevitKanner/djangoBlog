cc3876e4d5b05539db3d3314b8c4b52621c0858d
Levit Osei Kanner <lkanner21@gmail.com>
2019-01-01 13:10:32 +0000

    Fixes various bugs

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
+from blog.views import PostListView
 from django.urls import path
 from . import views
 
 app_name = 'blog'
 
-urlpatterns = [path('', views.post_list, name='post_list'),
+urlpatterns = [path('', views.PostListView.as_view(), name='post_list'),
                path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail')
                ]
