a2d0c025e51e2da4153c0e991adde9ab7df9b3dd
Levit Osei Kanner <lkanner21@gmail.com>
2020-10-22 16:49:14 +0000

    Builds a custom model manager for Post

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,12 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
+
+
 # Create your models here.
 class Post(models.Model):
     STATUS_CHOICES = (
@@ -17,8 +23,16 @@ class Post(models.Model):
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
     
+    #Managers
+    objects = models.Manager() # Default manager
+    published = PublishedManager()
+    
     class Meta:
         ordering = ('-publish',)
         
     def __str__(self):
         return self.title
+    
+    
+
+    
