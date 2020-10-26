d0516079f0bdbc9cdf2e0c70dfb38f9cc0d148b4
Levit Osei Kanner <lkanner21@gmail.com>
2020-10-26 19:44:47 +0000

    Adds a url pattern for paosts with a particular tag

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,10 @@
-from blog.views import PostListView
 from django.urls import path
 from . import views
 
 app_name = 'blog'
 
-urlpatterns = [path('', views.PostListView.as_view(), name='post_list'),
+urlpatterns = [path('', views.post_list, name='post_list'),
+               path('tag/<slug:tag_slug>', views.post_list, name='post_list_by_tag'),
                path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'), 
                path('<int:post_id>/share', views.post_share, name='post_share')
                ]
