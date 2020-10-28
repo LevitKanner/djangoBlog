57de507e80ed1f6c400fc1b4a9ee58b38ff83571
Levit Osei Kanner <lkanner21@gmail.com>
2020-10-28 15:33:53 +0000

    Fixes error in portfolio views

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from .models import Personal
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 def index(request):
-    info = Personal.objects.all()[0]
+    info = get_object_or_404(Personal, name='Levit Osei-Wusu')
     languages = info.languages.all()
     return render(request, 'portfolio/index.html', {'info': info, 'languages': languages})
