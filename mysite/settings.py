fceac641f4c77208595ab4839a342a1a911770d8
Levit Osei Kanner <lkanner21@gmail.com>
2020-10-29 17:28:42 +0000

    Adds django sitemap and sites framework to installed apps

--- mysite/settings.py
+++ mysite/settings.py
@@ -30,8 +30,11 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
+SITE_ID = 1
 
 INSTALLED_APPS = [
+    'django.contrib.sitemaps',
+    'django.contrib.sites',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
