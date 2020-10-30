89d3df5022da3ca27e0ce2ba43a426fcec8e2fcb
Levit Osei Kanner <lkanner21@gmail.com>
2020-10-30 11:28:40 +0000

    Adds RSS feed to blog

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from .feeds import LatestPostFeed
 from . import views
 
 app_name = 'blog'
@@ -6,5 +7,6 @@ app_name = 'blog'
 urlpatterns = [path('', views.post_list, name='post_list'),
                path('tag/<slug:tag_slug>', views.post_list, name='post_list_by_tag'),
                path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'), 
-               path('<int:post_id>/share', views.post_share, name='post_share')
+               path('<int:post_id>/share', views.post_share, name='post_share'),
+               path('feed/', LatestPostFeed(), name='posts_feed')
                ]
